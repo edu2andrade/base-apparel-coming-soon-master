5ab73227d7da9f88300504fc657626fe03d88de4
Eduardo Andrade <edu2andrade@gmail.com>
2021-10-13 13:07:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const emailValue = email.value
+
+    if(emailValue === '' || !validateEmail(emailValue)) {
+        form.className = 'form error'
+    } else {
+        form.className = 'form success'
+    }
+})
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLowerCase());
+}
