0dd596c427e5dfa6b0861f9b4542daab47876dcb
Eduardo Andrade <edu2andrade@gmail.com>
2021-10-13 15:32:59 +0200

    remove files & add README file

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const form = document.getElementById('form')
 const email = document.getElementById('email')
 
+// listening the submit event on form
 form.addEventListener('submit', e => {
     e.preventDefault()
     const emailValue = email.value
 
     if(emailValue === '' || !validateEmail(emailValue)) {
-        form.className = 'form error'
+        form.className = 'form error' // change class name on form
     } else {
-        form.className = 'form success'
+        form.className = 'form success' // change class name on form
     }
 })
-
+// function with REGEX for email validation...
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
